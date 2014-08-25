32da9e1da76f0d1aaedf36f3b1844999aee718fd
Shubham <pansarishubham2395@gmail.com>
2014-08-26 01:28:18 +0530

    heroku development

--- JobSeeker/settings.py
+++ JobSeeker/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'JobSeeker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
@@ -73,7 +73,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+'''
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
@@ -103,3 +103,24 @@ STATICFILES_FINDERS = (
 
 #templates directory
 TEMPLATE_DIRS = BASE_DIR+'/templates/'
+
+#settings for heroku
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- JobSeeker/wsgi.py
+++ JobSeeker/wsgi.py
@@ -11,4 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "JobSeeker.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+#for heroku
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
